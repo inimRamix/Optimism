ff2cb4f129419ab49464638c1b5e692566a08f6c
Ivan Afanasiev <Ram1zen@yandex.ru>
2025-07-20 22:42:06 +0700

    discount with celery

---
+++ service/services/tasks.py
@@ -0,0 +1,23 @@
+import time
+
+from celery import shared_task
+from celery_singleton import Singleton
+from django.db.models import F
+
+
+@shared_task(base=Singleton)
+def set_price(subscription_id):
+    from services.models import Subscription
+
+    time.sleep(10)
+
+    subscription = Subscription.objects.filter(id=subscription_id).annotate(
+        annotated_price=F('service__full_price') -
+              F('service__full_price') * F('plan__discount_percent') / 100.00).first()
+
+    # new_price = (subscription.service.full_price -
+    #              subscription.service.full_price * subscription.plan.discount_percent / 100)
+    # subscription.price = new_price
+
+    subscription.price = subscription.annotated_price
+    subscription.save()
